cd0ccda3c28356672cd9ffc523d1abce3978d108
Ashley Fisher <fish.ash@gmail.com>
2017-07-28 13:57:27 -0700

    added bootstrap and css file

--- sistersadmin/settings.py
+++ sistersadmin/settings.py
@@ -123,6 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     #This lets Django's collectstatic store our bundles
     os.path.join(BASE_DIR, 'assets'), 
+    os.path.join(BASE_DIR, 'static'), 
 )
 
 WEBPACK_LOADER = {
