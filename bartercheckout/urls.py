a2133d9e605f99a3bbc60ac4965abf1f16aa000c
razzlepdx <rwilson.or@gmail.com>
2017-07-28 11:46:07 -0700

    add wireframes for account, add credit, and buy meals

--- bartercheckout/urls.py
+++ bartercheckout/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^account/', views.account, name='account'),
+    url(r'^buymeal/', views.buymeal, name='buymeal'),
+    url(r'^addcredit/', views.addcredit, name='addcredit')
 ]
--- bartercheckout/views.py
+++ bartercheckout/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html', {})
+
+def account(request):
+    return render(request, 'account.html', {})
+
+def buymeal(request):
+    return render(request, 'buymeal.html', {})
+
+def addcredit(request):
+    return render(request, 'addcredit.html', {})
