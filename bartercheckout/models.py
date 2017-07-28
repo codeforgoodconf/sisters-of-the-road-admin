db19585bd33f2b9faae1a0bc1cc95fd5609b2fc2
Andrew Harvey <harveyam@whitman.edu>
2017-07-28 14:40:34 -0700

    Created models for BarterEvent and BarterAccount

--- bartercheckout/models.py
+++ bartercheckout/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class BarterEvent(models.Model):
+	barter_account = models.ForeignKey(
+		'BarterAccount',
+		#on_delete=models.CASCADE,
+	)
+	ADD = 'Add'
+	SUBTRACT = 'Subtract'
+	NOTE = 'Note'
+	EVENT_TYPE_CHOICES = (
+		(ADD, 'Add'),
+		(SUBTRACT, 'Subtract'),
+		(NOTE, 'Note'),
+	)
+	event_type = models.CharField(
+		max_length=20,
+		choices = event_type_choices,
+		default = SUBTRACT,
+	)
+	event_time = models.DateTimeField(auto_now_add=True)
+	#staff_id = models.ForeignKey()
+
+class BarterAccount(models.Model):
+	diner_name = models.CharField(max_length=100)
+	balance = models.IntegerField(max_length=30)
+	
