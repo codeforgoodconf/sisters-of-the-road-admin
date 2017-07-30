d3b39b72eb1756771fbc0e8223bccf9b76110dfc
Melissa Lewis <meli.lewis@gmail.com>
2017-07-30 12:09:59 -0700

    Add id to attributes returned

--- bartercheckout/views.py
+++ bartercheckout/views.py
@@ -19,10 +19,11 @@ def list_accounts(request):
     query_result = BarterAccount.objects.all()
     account_list = []
     for account in query_result:
-            account_dict = {'name': account.customer_name,
-                           'balance': account.balance,
-                           'last_add': account.last_add or 'Nothing yet!',
-                           'last_subtract': account.last_subtract or 'Nothing yet!'}
+            account_dict = {'account_id': account.id,
+                            'name': account.customer_name,
+                            'balance': account.balance,
+                            'last_add': account.last_add or 'Nothing yet!',
+                            'last_subtract': account.last_subtract or 'Nothing yet!'}
             account_list.append(account_dict)
     return JsonResponse(account_list, safe=False)
 
@@ -31,10 +32,11 @@ def search_accounts(request):
     query_result = BarterAccount.objects.filter(customer_name__icontains=search_text)
     account_list = []
     for account in query_result:
-            account_dict = {'name': account.customer_name,
-                           'balance': account.balance,
-                           'last_add': account.last_add or 'Nothing yet!',
-                           'last_subtract': account.last_subtract or 'Nothing yet!'}
+            account_dict = {'account_id': account.id,
+                            'name': account.customer_name,
+                            'balance': account.balance,
+                            'last_add': account.last_add or 'Nothing yet!',
+                            'last_subtract': account.last_subtract or 'Nothing yet!'}
             account_list.append(account_dict)
     return JsonResponse(account_list, safe=False)
 
