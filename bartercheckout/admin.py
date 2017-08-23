024a4c349d7de41f66387982cffbddb0096f4475
razzlepdx <rwilson.or@gmail.com>
2017-08-23 08:33:41 -0700

    changing Django Admin headers

--- bartercheckout/admin.py
+++ bartercheckout/admin.py
@@ -14,6 +14,7 @@ class BarterAccountAdmin(admin.ModelAdmin):
 	"""
 	This is where you modify the view of a BarterAccount in the admin page.
 	"""
-
+admin.site.site_header = 'Sisters of the Road Cafe Admin'
+admin.site.index_title = 'Sisters of the Road Checkout Administration'
 admin.site.register(BarterAccount)
 admin.site.register(BarterEvent)
