3dfbd3d2cc8ce0cb1fa5d69ea7de1b97f0d207fd
Ashley Fisher <fish.ash@gmail.com>
2017-08-27 13:20:22 -0700

    search button and list update

--- assets/js/SearchPage.js
+++ assets/js/SearchPage.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
 
 
+
 class SearchPage extends Component {
     constructor () {
         super();
+        this.state = {
+            accounts: [],
+            searchQuery: ''
+        };
+    }
+
+    onSearchChange (value) {
+        this.setState({searchQuery: value});
+    }
+
+    searchAccounts () {
+        axios.get('/account/search?q=' + this.state.searchQuery).then((response) => {
+            let accounts = [];
+            response.data.forEach((account, index) => {
+                accounts.push({
+                    name: account.name,
+                    id: account.account_id,
+                    lastCredit: account.last_add,
+                    lastMeal: account.last_subtract,
+                    currentCredit: Number(account.balance)
+                });
+            });
+            this.setState({accounts: accounts});
+        });
     }
 
     render () {
         const {
-            accounts
-        } = this.props;
+            accounts,
+            searchQuery
+        } = this.state;
         return (
             <div className="SearchPage">
                 <header>
                     <h1 className="text-center">Sisters of the Road Cafe</h1>
                 </header>
                 <div id="seachbar">
-                    <input className="input-lg center-block" type="text" placeholder="Search for patron"/>
+                    <input className="input-lg center-block"
+                           type="text"
+                           placeholder="Search for patron"
+                           onChange={(event) => this.onSearchChange(event.target.value)}/>
+                    
+                </div>
+                <div className="row">
+                    <button className="btn btn-info col-sm-offset-5 center-block"
+                            onClick={() => this.searchAccounts(searchQuery)}>
+                        Search
+                    </button>
                 </div>
                 <div className="row">
                 {accounts.map((account) =>
