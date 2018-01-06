a5fd7af18939cb6270420905861645753a1bd66f
Juliana Arrighi <juliana.arrighi@gmail.com>
2018-01-06 14:37:55 -0800

    wsgi file update for white noise

--- sistersadmin/wsgi.py
+++ sistersadmin/wsgi.py
@@ -14,4 +14,4 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sistersadmin.settings")
 
-application = get_wsgi_application()
+application = DjangoWhiteNoise(get_wsgi_application())
