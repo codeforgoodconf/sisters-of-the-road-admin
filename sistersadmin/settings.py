6d1e0d02b38143d763c3ac9ec0135095af448dc3
Sharlee Bryan <26746468+sharz1@users.noreply.github.com>
2018-01-20 15:50:38 -0800

    modified dollars display in admin. Changes not complete, pushing to work on changes on another machine.

--- sistersadmin/settings.py
+++ sistersadmin/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'sistersadmin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'barter',
-        'USER': 'sisters',
-        'PASSWORD': 'sisters',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
