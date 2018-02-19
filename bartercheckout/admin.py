3e3f1b0137bdaf34b7831c0533352e539aa0be17
sbryan <26746468+sharz1@users.noreply.github.com>
2018-02-18 16:00:09 -0800

    removes extra lines and comments

--- bartercheckout/admin.py
+++ bartercheckout/admin.py
@@ -15,19 +15,9 @@ class BarterAccountAdmin(admin.ModelAdmin):
 	This is where you modify the view of a BarterAccount in the admin page.
 	"""
 	list_display = ['customer_name', 'account_balance']
-	
-	
 	fields = ['customer_name','balance','last_add','last_subtract']
 	readonly_fields = ['balance','last_add','last_subtract']
 	
-	
-
-	
-	"""
-	def account_balance(self, obj):
-		return '${:,.2f}'.format(obj.balance/100)
-	"""
-
 admin.site.site_header = 'Sisters of the Road Cafe Admin'
 admin.site.index_title = 'Sisters of the Road Checkout Administration'
 admin.site.register(BarterAccount, BarterAccountAdmin)
