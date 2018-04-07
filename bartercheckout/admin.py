958c9f180ec921e65216ac03e08eeab2e9e3d4a7
Schola Choi <scholachoi@gmail.com>
2018-04-07 16:31:38 -0700

    Put back docstrings

--- bartercheckout/admin.py
+++ bartercheckout/admin.py
@@ -6,6 +6,9 @@ from .models import BarterAccount
 
 
 class BarterEventAdmin(admin.ModelAdmin):
+	"""
+	This is where you modify the view of a BarterEvent in the admin page.
+	"""
 	list_display = [
 		'event_id',
 		'customer_name',
@@ -21,6 +24,9 @@ class BarterEventAdmin(admin.ModelAdmin):
 
 
 class BarterAccountAdmin(admin.ModelAdmin):
+	"""
+	This is where you modify the view of a BarterAccount in the admin page.
+	"""
 	list_display = ['customer_name', 'account_balance']
 	fields = ['customer_name','balance','last_add','last_subtract']
 	readonly_fields = ['last_add','last_subtract']
@@ -28,4 +34,4 @@ class BarterAccountAdmin(admin.ModelAdmin):
 admin.site.site_header = 'Sisters of the Road Cafe Admin'
 admin.site.index_title = 'Sisters of the Road Checkout Administration'
 admin.site.register(BarterAccount, BarterAccountAdmin)
-admin.site.register(BarterEvent, BarterEventAdmin) 
+admin.site.register(BarterEvent, BarterEventAdmin)
