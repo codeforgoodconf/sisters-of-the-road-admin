2a29545b1a3715b2236d579a1356864acf7c1199
Schola Choi <scholachoi@gmail.com>
2018-06-16 15:43:58 -0700

    Make buttons taller

--- assets/js/AccountPage.js
+++ assets/js/AccountPage.js
@@ -15,16 +15,16 @@ class AccountPage extends Component {
             <div class="AccountPage">
                 <AccountSummary account={account} switchView={this.props.switchView}/>
 
-                <div class="mt5 fr w-50">
-                    <button class="f3 br0 ph3 pv0 mb4 db h3 w-80 white bg-purple"
+                <div class="mt5 fr w-50 vh-25">
+                    <button class="f3 br0 ph3 pv0 mb4 db h-25 w-80 white bg-purple"
                             onClick={() => this.props.switchView('buycardpage', account)}>
                         <i class="far fa-credit-card pr2"></i>Buy Card 
                     </button>
-                    <button class="f3 br0 ph3 pv1 mb4 db h3 w-80 white bg-purple"
+                    <button class="f3 br0 ph3 pv1 mb4 db h-25 w-80 white bg-purple"
                             onClick={() => this.props.switchView('buymealpage', account)}>
                         <i class="fas fa-utensils pr2"></i>Buy Meal
                     </button>
-                    <button class="f3 br0 ph3 pv1 mb0 db h3 w-80 white bg-green"
+                    <button class="f3 br0 ph3 pv1 mb0 db h-25 w-80 white bg-green"
                             onClick={() => this.props.switchView('addcreditpage', account)}>
                         <i class="fas fa-plus pr2"></i>Add Credit
                     </button>
