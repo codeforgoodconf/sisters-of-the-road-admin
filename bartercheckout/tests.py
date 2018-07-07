4b0b04372afe8765005376328cb2377cfa5d00de
sbryan <26746468+sharz1@users.noreply.github.com>
2018-07-07 15:16:40 -0700

    adds error response tests

--- bartercheckout/tests.py
+++ bartercheckout/tests.py
@@ -18,34 +18,39 @@ class CreditTest(TestCase):
         self.factory = RequestFactory()
 
     def test_BarterAccount_exists_and_adds(self):
+        """
+        Test that user exists.
+        """
         summer = BarterAccount.objects.get(customer_name="Summer Salt")
         self.assertTrue(summer)
+        """
+        Test that the add() method was called.
+        """
         summers_id = summer.id
         request = self.factory.post('/account/{}/credit'.format(summers_id), json.dumps({'amount': 250}), content_type='application/json')
         response = credit(request, summers_id)
-        print(response)
-        """
-        If user exists test that the add() method is called.
-        """
         summer.refresh_from_db()
         self.assertEquals(summer.balance, 450)
         """
-        If exists, credit amount valid, no errors, credit event created.
+        Test that a credit event was created.
         """
         creditevent = BarterEvent.objects.get(event_type="Add", amount=250)
         self.assertTrue(creditevent)
         """
         If exists, credit amount NOT valid, json returned describing error.
         """
+        request = self.factory.post('/account/{}/credit'.format(summers_id), json.dumps({'amount': 255}), content_type='application/json')
+        response = credit(request, summers_id)
+        self.assertJSONEqual(response.content, {'result': 'input_error'})
+        summer.refresh_from_db()
+        self.assertEquals(summer.balance, 450)
         
-        
-    def test_invalid_BarterAccount(self):
+
+    def test_error_invalid_BarterAccount(self):
         """
         If user does not exist, test proper error response returned.
         """
-
-
-        """
-        response = self.credit()
-        """
+        request = self.factory.post('/account/{}/credit'.format(998), json.dumps({'amount': 250}), content_type='application/json')
+        response = credit(request, 998)
+        self.assertJSONEqual(response.content, {'error': 'noSuchAccount'})
     
