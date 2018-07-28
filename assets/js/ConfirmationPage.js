aa5d0dcef2b81cf8ff6d963907e16f2a8dca2c45
Luci Peterson <lucijpeterson@gmail.com>
2018-07-28 16:15:46 -0700

    UI changes and search functionality improvements
    Fixes issues #93 and #94

--- assets/js/ConfirmationPage.js
+++ assets/js/ConfirmationPage.js
@@ -16,7 +16,7 @@ class ConfirmationPage extends Component {
             <div class="ConfirmationPage">
                 <AccountSummary account={account} switchView={this.props.switchView}/>
                 <div id="calculate" class="fl w-50 mt5 ba bw1 pa2">
-                    <div class="total">
+                    <div class="total"><p class='f1'>Changes Saved!</p>
                         {account.lastAdded > 0 &&
                             <h2 class="tl">Amount added: ${(account.lastAdded / 100).toFixed(2)}</h2>
                         }
@@ -26,6 +26,12 @@ class ConfirmationPage extends Component {
 
                         <h2 class="tl">New balance: ${(account.currentCredit / 100).toFixed(2)}</h2>
                     </div>
+                </div>
+                <div class="mt5 fr w-50 h-50">
+                    <button class="f3 br0 ph3 pv0 mb4 db h-25 w-80 white bg-purple"
+                            onClick={() => this.props.switchView('accountpage', account)}>
+                        <i class="far pr2"></i>Back to Account Summary 
+                    </button>
                 </div>
             </div>
          );
