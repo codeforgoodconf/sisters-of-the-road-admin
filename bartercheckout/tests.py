e23f2d657d55260f310131f18e09eb6fa33badfd
sbryan <26746468+sharz1@users.noreply.github.com>
2018-08-11 15:29:47 -0700

    converts multiline comments to single

--- bartercheckout/tests.py
+++ bartercheckout/tests.py
@@ -9,36 +9,26 @@ from .views import credit
 class CreditTest(TestCase):
     
     def setUp(self):
-        """
-        Add account to db
-        """
+        # Add account to db
         summer = BarterAccount.objects.create(customer_name="Summer Salt", balance=200)
         summer.save()
         
         self.factory = RequestFactory()
 
     def test_BarterAccount_exists_and_adds(self):
-        """
-        Test that user exists.
-        """
+        # Test that user exists.
         summer = BarterAccount.objects.get(customer_name="Summer Salt")
         self.assertTrue(summer)
-        """
-        Test that the add() method was called.
-        """
+        # Test that the add() method was called.
         summers_id = summer.id
         request = self.factory.post('/account/{}/credit'.format(summers_id), json.dumps({'amount': 250}), content_type='application/json')
         response = credit(request, summers_id)
         summer.refresh_from_db()
         self.assertEquals(summer.balance, 450)
-        """
-        Test that a credit event was created.
-        """
+        # Test that a credit event was created.
         creditevent = BarterEvent.objects.get(event_type="Add", amount=250)
         self.assertTrue(creditevent)
-        """
-        If exists, credit amount NOT valid, json returned describing error.
-        """
+        # If exists, credit amount NOT valid, json returned describing error.
         request = self.factory.post('/account/{}/credit'.format(summers_id), json.dumps({'amount': 255}), content_type='application/json')
         response = credit(request, summers_id)
         self.assertJSONEqual(response.content, {'result': 'input_error'})
@@ -47,9 +37,7 @@ class CreditTest(TestCase):
         
 
     def test_error_invalid_BarterAccount(self):
-        """
-        If user does not exist, test proper error response returned.
-        """
+        # If user does not exist, test proper error response returned.
         request = self.factory.post('/account/{}/credit'.format(998), json.dumps({'amount': 250}), content_type='application/json')
         response = credit(request, 998)
         self.assertJSONEqual(response.content, {'error': 'noSuchAccount'})
