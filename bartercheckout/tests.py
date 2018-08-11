10024aa1434b3ad13b3ecf27d29dc496ca03b229
Trevor <trevorbshepard@gmail.com>
2018-08-11 14:51:53 -0700

    test case for buy

--- bartercheckout/tests.py
+++ bartercheckout/tests.py
@@ -1,3 +1,56 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from mixer.backend.django import mixer
+
+from .models import BarterAccount
+
+
+class TestBuyMeal(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.barter_account = mixer.blend(BarterAccount)
+
+    def test_valid_transaction(self):
+        self.barter_account.balance = 150
+        self.barter_account.save()
+
+        response = self.client.post(
+            reverse('buy_meal', kwargs={'account_id': self.barter_account.id}),
+            data=json.dumps({
+                'amount': 100
+            }),
+            content_type='application/json'
+        )
+        
+        self.barter_account.refresh_from_db()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'{"result": "ok"}')
+        self.assertEqual(self.barter_account.balance, 50)
+
+    def test_invalid_transaction(self):
+        response = self.client.post(
+            reverse('buy_meal', kwargs={'account_id': self.barter_account.id}),
+            data=json.dumps({
+                'amount': 100
+            }),
+            content_type='application/json'
+        )
+        
+        self.assertEqual(response.status_code, 402)
+        self.assertEqual(response.content, b'{"result": "limit_error"}')
+
+    def test_invalid_input_transaction(self):
+        response = self.client.post(
+            reverse('buy_meal', kwargs={'account_id': self.barter_account.id}),
+            data=json.dumps({
+                'amount': 33
+            }),
+            content_type='application/json'
+        )
+        
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.content, b'{"result": "input_error"}')
