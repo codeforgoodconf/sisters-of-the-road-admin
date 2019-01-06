f995a8c365d89295e952287a122d47515fc2958e
Mike Lane <mikelane@gmail.com>
2019-01-05 19:59:45 -0800

    109 Use dollars and cents in the account model

--- bartercheckout/models.py
+++ bartercheckout/models.py
@@ -1,12 +1,18 @@
-from django.db import models
+import decimal
 from datetime import date
+
+from django.db import models
+
+decimal.getcontext().prec = 2
 
 
 class BalanceLimitError(Exception):
     """Raise when account balance exceeds limits"""
 
+
 class AmountInputError(Exception):
     """Raise when amount entered is invalid"""
+
 
 # Create your models here.
 class BarterEvent(models.Model):
@@ -22,13 +28,13 @@ class BarterEvent(models.Model):
         (BUY_MEAL, 'Buy_meal'),
         (BUY_CARD, 'Buy_card'),
         (NOTE, 'Note'),
-        )
+    )
 
     event_type = models.CharField(
         max_length=20,
-        choices = EVENT_TYPE_CHOICES,
-        default = BUY_MEAL,
-        )
+        choices=EVENT_TYPE_CHOICES,
+        default=BUY_MEAL,
+    )
 
     event_time = models.DateTimeField(auto_now_add=True)
     amount = models.IntegerField(default=0)
@@ -40,44 +46,58 @@ class BarterEvent(models.Model):
     @property
     def transaction_amount(self):
         assert type(self.amount) == int
-        return '${:,.2f}'.format(self.amount/100)
+        return f'${self.amount:,.2f}'
 
-    #staff_id = models.ForeignKey()
-    
+    # staff_id = models.ForeignKey()
+
     def __str__(self):
-        return 'Barter event ID {} for {}'.format(self.id, self.customer_name)
+        return f'Barter event ID {self.id} for {self.customer_name}'
 
 
 class BarterAccount(models.Model):
     customer_name = models.CharField(max_length=100)
-    balance = models.IntegerField(default=0)
+    balance = models.DecimalField(max_digits=4, decimal_places=2, default=0.0)
     last_add = models.DateField(null=True)
     last_subtract = models.DateField(null=True)
 
-    def add(self, amount): 
-        if amount < 0 or amount % 25 != 0:
+    def add(self, amount):
+        if amount < 0 or (amount * 100) % 25 != 0:
             raise AmountInputError("Invalid amount")
-        if self.balance + amount > 5000:
-            raise BalanceLimitError("Balance can't go above $50")
+
+        dec_amount = decimal.Decimal(amount)
+        if self.balance + dec_amount > 50:
+            raise BalanceLimitError(f"Balance can't go above $50. Current balance ${self.balance}. You can add up to "
+                                    f"${decimal.Decimal(50.0) - self.balance}")
         else:
-            self.balance += amount
+            self.balance += dec_amount
             self.last_add = date.today()
             return self.balance
 
     def subtract(self, amount):
         if amount < 0 or amount % 25 != 0:
             raise AmountInputError("Invalid amount")
-        if self.balance - amount < 0:
+
+        dec_amount = decimal.Decimal(amount)
+        if self.balance - dec_amount < 0:
             raise BalanceLimitError("Balance can't go below $0")
         else:
-            self.balance -= amount
+            self.balance -= dec_amount
             self.last_subtract = date.today()
             return self.balance
 
+
+    def __repr__(self):
+        return (
+            f'BarterAccount(customer_name="{self.customer_name}", '
+            f'balance={self.balance}, '
+            f'last_add={self.last_add}, '
+            f'last_subtract={self.last_subtract})'
+        )
+
     def __str__(self):
-        return 'Account: {}'.format(self.customer_name)
+        return f'Account: {self.customer_name}'
 
     @property
     def account_balance(self):
         assert type(self.balance) == int
-        return '${:,.2f}'.format(self.balance/100) if self.balance else ""
+        return f'${self.balance:,.2f}' if self.balance else ''
--- bartercheckout/tests.py
+++ bartercheckout/tests.py
@@ -1,43 +1,50 @@
 import json
+
 from django.test import TestCase
-from .models import BarterEvent, BarterAccount
 from django.test.client import RequestFactory
+
+from .models import BarterAccount, BarterEvent
 from .views import credit
 
 
 # Create your tests here.
 class CreditTest(TestCase):
-    
+
     def setUp(self):
         # Add account to db
-        summer = BarterAccount.objects.create(customer_name="Summer Salt", balance=200)
+        summer = BarterAccount.objects.create(customer_name='Summer Salt', balance=2.00)
         summer.save()
-        
+
         self.factory = RequestFactory()
 
     def test_BarterAccount_exists_and_adds(self):
         # Test that user exists.
-        summer = BarterAccount.objects.get(customer_name="Summer Salt")
+        summer = BarterAccount.objects.get(customer_name='Summer Salt')
         # Test that the add() method was called.
         summers_id = summer.id
-        request = self.factory.post('/account/{}/credit'.format(summers_id), json.dumps({'amount': 250}), content_type='application/json')
+        request = self.factory.post(f'/account/{summers_id}/credit',
+                                    json.dumps({'amount': 2.50}),
+                                    content_type='application/json')
         response = credit(request, summers_id)
         summer.refresh_from_db()
-        self.assertEquals(summer.balance, 450)
+        self.assertEquals(summer.balance, 4.50)
         # Test that a credit event was created.
-        creditevent = BarterEvent.objects.get(event_type="Add", amount=250)
+        creditevent = BarterEvent.objects.get(event_type='Add', amount=2.50)
         self.assertTrue(creditevent)
         # If exists, credit amount NOT valid, json returned describing error.
-        request = self.factory.post('/account/{}/credit'.format(summers_id), json.dumps({'amount': 255}), content_type='application/json')
+        request = self.factory.post(f'/account/{summers_id}/credit',
+                                    json.dumps({'amount': 2.55}),
+                                    content_type='application/json')
         response = credit(request, summers_id)
         self.assertJSONEqual(response.content, {'result': 'input_error'})
         summer.refresh_from_db()
-        self.assertEquals(summer.balance, 450)
-        
+        self.assertEquals(summer.balance, 4.50)
 
     def test_error_invalid_BarterAccount(self):
         # If user does not exist, test proper error response returned.
-        request = self.factory.post('/account/{}/credit'.format(998), json.dumps({'amount': 250}), content_type='application/json')
-        response = credit(request, 998)
+        fake_id = 998
+        request = self.factory.post(f'/account/{fake_id}/credit',
+                                    json.dumps({'amount': 2.50}),
+                                    content_type='application/json')
+        response = credit(request, fake_id)
         self.assertJSONEqual(response.content, {'error': 'noSuchAccount'})
-    
